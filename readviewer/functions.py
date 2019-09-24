29f65f151f43423f65c0c43087b3b4fa62f3c979
gariepinus <mail@gariepinus.de>
2019-09-24 20:02:44 +0200

    Added cumulate function

--- readviewer/functions.py
+++ readviewer/functions.py
@@ -1,4 +1,6 @@
 from statistics import mean
+from functools import reduce
+from datetime import timedelta
 import readviewer.data as data
 
 def calculate_scores():
@@ -15,3 +17,28 @@ def calculate_scores():
         pages_score = session.pages / one_percent_pages
 
         session.set_score(int(mean([duration_score, pages_score])))
+
+def cumulate(stat, day=None,month=None,year=None):
+    """Return the cumulative score, session number, pages, duration or average speed for one day month or year or all sessions."""
+
+    if day is not None:
+        lst = list(filter(lambda session: (str(session.timestamp.date()) == day), data.sessions))
+    elif month is not None:
+        lst = list(filter(lambda session: ("{}-{:0>2}".format(session.timestamp.year, session.timestamp.month) == month), data.sessions))
+    elif year is not None:
+        lst = list(filter(lambda session: (str(session.timestamp.year) == year), data.sessions))
+    else:
+        lst = data.sessions
+
+    if stat == "score":
+        return int(sum([session.score for session in lst]))
+    elif stat == "sessions":
+        return len(lst)
+    elif stat == "pages":
+        return sum([session.pages for session in lst])
+    elif stat == "duration":
+        return reduce(lambda a,b: a + b.duration, lst, timedelta())
+    elif stat == "speed" and len(lst) > 0:
+        return int(mean([session.speed for session in lst]))
+    else:
+        return 0
