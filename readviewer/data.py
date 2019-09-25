e4144b1cf0c245b752d4563d54097e519fb3ddf2
gariepinus <mail@gariepinus.de>
2019-09-25 18:56:38 +0200

    Moved load funtion into data module

--- readviewer/data.py
+++ readviewer/data.py
@@ -1,12 +1,18 @@
 import json
 from functools import reduce
+from statistics import mean
 from readviewer.models import Session, Book
+
 
 books = []
 sessions = []
 
 
 def load(file):
+    """Load ReadTracker export data."""
+
+    global books, sessions
+
     with open(file, "r") as data_file:
         export_data = json.load(data_file)
 
@@ -15,7 +21,26 @@ def load(file):
     
     # Collect the sessions from all books in a list and calculate their scores
     sessions = reduce(lambda a,b: a + b.sessions, books, [])
-    #calculate_scores()
+    calculate_scores()
 
+    # Sort the lists
     books.sort(key = lambda book: book.current_position_timestamp, reverse=True)
     sessions.sort(key = lambda session: session.timestamp, reverse=True)
+
+
+def calculate_scores():
+    """Calculate session scores and save add them to the instances."""
+
+    global books, sessions
+
+    max_duration = max([session.duration for session in sessions])
+    max_pages = max([session.pages for session in sessions])
+
+    one_percent_duration = max_duration / 100
+    one_percent_pages = max_pages / 100
+
+    for session in sessions:
+        duration_score = session.duration / one_percent_duration
+        pages_score = session.pages / one_percent_pages
+
+        session.set_score(int(mean([duration_score, pages_score])))
--- readviewer/functions.py
+++ readviewer/functions.py
@@ -3,21 +3,6 @@ from functools import reduce
 from datetime import timedelta
 import readviewer.data as data
 
-def calculate_scores():
-    """Calculate every sessions score."""
-
-    max_duration = max([session.duration for session in data.sessions])
-    max_pages = max([session.pages for session in data.sessions])
-
-    one_percent_duration = max_duration / 100
-    one_percent_pages = max_pages / 100
-
-    for session in data.sessions:
-        duration_score = session.duration / one_percent_duration
-        pages_score = session.pages / one_percent_pages
-
-        session.set_score(int(mean([duration_score, pages_score])))
-
 def cumulate(stat, day=None,month=None,year=None):
     """Return the cumulative score, session number, pages, duration or average speed for one day month or year or all sessions."""
 
