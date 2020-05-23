5eda9f867281ace3aa0d144b1770d7e108cf873d
gariepinus <mail@gariepinus.de>
2020-05-23 17:18:42 +0200

    Using argparse for cli arguments

--- readviewer/__main__.py
+++ readviewer/__main__.py
@@ -1,29 +1,26 @@
 import sys
+import argparse
 import readviewer.data as data
-#import readviewer.ui as ui
+# import readviewer.ui as ui
 
 if __name__ == "__main__":
 
     #
     # Eval cli arguments
     #
-    usageinfo = "readviewer <PATH>"
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "help":
-            print(usageinfo)
-            exit(0)
-        else:
-            file_path = sys.argv[1]
-    else:
-        print(usageinfo)
-        exit(1)
+    parser = argparse.ArgumentParser(description="""Visualize
+                         ReadTracker data in your terminal.""")
+    parser.add_argument('file', metavar='FILE', type=str,
+                        help="""the exported
+                         JSON data you want to view""")
+    args = parser.parse_args()
 
     #
     # Load json data
     #
-    data.load(file_path)
+    data.load(args.file)
 
     #
     # Start UI loop
     #
-    #ui.run()
+    # ui.run()
