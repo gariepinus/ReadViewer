cd795da5cb9911a6d139f51fe35cfd018905d9d3
gariepinus <mail@gariepinus.de>
2020-07-03 13:34:08 +0200

    Removed depricated code

--- readviewer/database.py
+++ readviewer/database.py
@@ -1,11 +1,8 @@
 import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from readviewer.models import Base, Reading_Session, Session_list, Book, Book_list
+from readviewer.models import Base, Reading_Session, Book
 
-
-books = Book_list()
-sessions = Session_list()
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Session = sessionmaker(bind=engine)
@@ -14,22 +11,15 @@ session = Session()
 
 def load(file):
     """Load ReadTracker export data into in-memory-only SQLite database."""
-    global books, sessions
     global engine, session
-
     Base.metadata.create_all(engine)
 
     with open(file, "r") as data_file:
         export_data = json.load(data_file)
 
     for book in export_data['books']:
-        books.append(Book(book))
-        sessions += Book(book)
-
         session.add(Book(book))
         for reading_session in book['sessions']:
             session.add(Reading_Session(reading_session, book['page_count']))
 
-    books.sort("current_position_timestamp", reverse=True)
-    sessions.sort("timestamp", reverse=True)
     session.commit()
